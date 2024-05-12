c00211c26b5f858d302992afc692a61cc6444ffc
Christian Boswell <christianm.boswell@gmail.com>
2024-05-11 20:38:56 -0500

    Bug fixes and clean up

--- main.py
+++ main.py
@@ -6,40 +6,64 @@ import statistics
 # Initialize Pygame
 pygame.init()
 
-# Set up some constants
-WIDTH, HEIGHT = 640, 480
+WIDTH, HEIGHT = 960, 360
 RADIUS = 5
 COLOR = (255, 0, 0)  # Red
 
-# Create a font object
+# Create a font object for avg distance to cluster center
 font = pygame.font.Font(None, 36)
 
 # Set up the display
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# Ask for the number of circles
-num_circles = int(input("Enter the number of circles: "))
+while True:
+    try:
+        num_data_points = int(input("Enter the number of data points: "))
+        if num_data_points < 0:
+            print('Please Choose a non-negative value for the number of data points')
+        else: 
+            break
+    except:
+        print('Please Choose a pick a new number of data points')
+        
+while True:
+    try:
+        num_clusters = int(input("Enter the number of clusters: "))
+        if num_clusters < 0:
+            print('Please Choose a non-negative value for the number of clusters')
+        else: 
+            break
+    except:
+        print('Please Choose a pick a new number of clusters')
 
-# Ask for the number of K-means
-num_kmeans = int(input("Enter the number of K-means: "))
+print('The centroid convergence speed goes up as the input value goes down, speed of 1 being the fastest')
+while True:
+    try:
+        speed_of_convergence = int(input("Enter the desired speed of convergence: "))
+        if speed_of_convergence < 0:
+            print('Please Choose a non-negative value for the speed of convergence')
+        else: 
+            break
+    except:
+        print('Please Choose a pick a new value for the speed')
 
-# Ask for the number of K-means
-speed_of_convergence = int(input("Enter the speed of convergence: "))
 
-# Generate the circles
-circles = []
-for _ in range(num_circles):
+# Generate the datapoints
+data_points = []
+for _ in range(num_data_points):
     x = random.randint(RADIUS, WIDTH - RADIUS)
     y = random.randint(RADIUS, HEIGHT - RADIUS)
-    circles.append((x, y))
+    data_points.append((x, y))
 
-# Generate the K-means and their border colors
+# Generate the clusters and their border colors
 kmeans = []
 border_colors = []
 data_point_clusters = []
-for _ in range(num_kmeans):
-    x = random.randint(RADIUS, WIDTH - RADIUS)
-    y = random.randint(RADIUS, HEIGHT - RADIUS)
+for _ in range(num_clusters):
+    # x = random.randint(RADIUS, WIDTH - RADIUS)
+    # y = random.randint(RADIUS, HEIGHT - RADIUS)
+    x = random.randint(RADIUS, 50 - RADIUS)
+    y = random.randint(RADIUS, 50 - RADIUS)
     kmeans.append((x, y))
     border_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
     border_colors.append(border_color)
@@ -52,62 +76,53 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # White background
     screen.fill((255, 255, 255))
 
     data_point_distances = []
     
-    for circle in circles:
+    for data_point in data_points:
         closest_kmean_index = 0
         closest_distance = float('inf')
         for i, kmean in enumerate(kmeans):
-            distance = math.sqrt((circle[0] - kmean[0]) ** 2 + (circle[1] - kmean[1]) ** 2)
+            distance = math.sqrt((data_point[0] - kmean[0]) ** 2 + (data_point[1] - kmean[1]) ** 2)
             if distance < closest_distance:
                 closest_distance = distance
                 closest_kmean_index = i
-        data_point_clusters[closest_kmean_index].append(circle)
+        data_point_clusters[closest_kmean_index].append(data_point)
         data_point_distances.append(closest_distance)
-        pygame.draw.circle(screen, border_colors[closest_kmean_index], circle, RADIUS)
+        pygame.draw.circle(screen, border_colors[closest_kmean_index], data_point, RADIUS)
     new_kmeans = []
     for i, kmean in enumerate(kmeans):
+        data_point_total = len(data_point_clusters[i])
         if data_point_total > 0:
             x_total = y_total = 0
-            data_point_total = len(data_point_clusters[i])
             for data_point in data_point_clusters[i]:
                 x_total = x_total + data_point[0]
                 y_total = y_total + data_point[1]
             new_kmean = (x_total / data_point_total, y_total / data_point_total)
             new_kmeans.append(new_kmean)
     
-            # Move the kmean towards the new position
             x = (new_kmean[0] - kmean[0]) / speed_of_convergence
             y = (new_kmean[1] - kmean[1]) / speed_of_convergence
             
+            # Move the cluster centroid towards the new center
             kmean = (kmean[0] + x, kmean[1] + y)
+            
         kmeans[i] = kmean
             
         pygame.draw.circle(screen, border_colors[i], kmean, RADIUS + 2)
         pygame.draw.circle(screen, (0, 0, 0), kmean, RADIUS - 1)
 
-    # kmeans = new_kmeans
-    data_point_clusters = [[] for _ in range(num_kmeans)]
+    data_point_clusters = [[] for _ in range(num_clusters)]
 
-    # Render the text
+    # Draw avg distance
     text = font.render("Avg Dist: " + str(round(statistics.mean(data_point_distances), 2)), True, (0, 0, 0))
-
-    # Calculate the size of the text
     text_rect = text.get_rect()
-
-    # Draw a white rectangle behind the text
     pygame.draw.rect(screen, (255, 255, 255), (10, 10, text_rect.width, text_rect.height))
-
-    screen.blit(text, (10, 10))  # Position the text at (10, 10)
+    screen.blit(text, (10, 10)) 
     
-    # Flip the display
     pygame.display.flip()
 
-    # Cap the frame rate
     pygame.time.delay(100)
 
-# Quit Pygame
 pygame.quit()
